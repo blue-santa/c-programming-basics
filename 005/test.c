559bd918e4347d51c6e73dbbebed8d6f7b6cf2fd
siddhartha <siddhartha-crypto@protonmail.com>
2019-12-31 14:26:23 -0900

    begin 005

---
+++ 005/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() { 
+    int integerVal = 100;
+    float floatingVal = 331.79;
+    double doubleVar = 8.44e+11;
+    char charVal = 'W';
+
+    _Bool boolVar = 0;
+
+    printf("integerVal = %i\n", integerVal);
+    printf("floatingVal = %f\n", floatingVal);
+    printf("doubleVar = %e\n", doubleVar);
+    printf("doubleVar= %g\n", doubleVar);
+    printf("charVal = %c\n", charVal);
+
+    printf("boolVar = %i\n", boolVar);
+
+    return 0; 
+}
