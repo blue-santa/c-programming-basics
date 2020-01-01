841d7fe1a22f711026a86c646b27ec5eca8de178
siddhartha <siddhartha-crypto@protonmail.com>
2019-12-31 21:23:22 -0900

    update to tax bracket challenge

--- 013/test.c
+++ 013/test.c
@@ -20,29 +20,29 @@ int main(int argc, char *argv[]) {
 
     double payRate = 12.0;
 
-    double grossPay = (normalHours * payRate) + (overtimeHours * payRate * 1.5);
-    double initialPay = grossPay;
+    double runningPay = (normalHours * payRate) + (overtimeHours * payRate * 1.5);
+    double grosspay = runningPay;
 
     double totalTax = 0.0;
 
-    if (grossPay > 0) {
-        grossPay > 300 ? (totalTax += 300 * 0.15) : (totalTax += grossPay * 0.15);
-        grossPay -= 300;
+    if (runningPay > 0) {
+        runningPay > 300 ? (totalTax += 300 * 0.15) : (totalTax += runningPay * 0.15);
+        runningPay -= 300;
     }
 
-    if (grossPay > 0) {
-        grossPay > 150 ? (totalTax += 150 * 0.2) : (totalTax += grossPay * 0.2);
-        grossPay -= 150;
+    if (runningPay > 0) {
+        runningPay > 150 ? (totalTax += 150 * 0.2) : (totalTax += runningPay * 0.2);
+        runningPay -= 150;
     }
 
-    if (grossPay > 0) {
-        totalTax += grossPay * 0.25;
+    if (runningPay > 0) {
+        totalTax += runningPay * 0.25;
     }
 
     printf("Normal Time: %f\n", normalHours);
     printf("Overtime: %f\n", overtimeHours);
-    printf("Gross Pay: %f\n", initialPay);
+    printf("Gross Pay: %f\n", grosspay);
     printf("Tax: %f\n", totalTax);
-    printf("Net Earnings: %f\n", initialPay - totalTax);
+    printf("Net Earnings: %f\n", grosspay - totalTax);
     return 0; 
 }
