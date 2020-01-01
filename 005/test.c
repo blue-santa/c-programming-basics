93b46741b46c5ca91e4e69862352b40924c6e3d6
siddhartha <siddhartha-crypto@protonmail.com>
2019-12-31 16:13:06 -0900

    initiate rectangle challenge

--- 005/test.c
+++ 005/test.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
 
 int main() { 
-    int integerVal = 100;
-    float floatingVal = 331.79;
-    double doubleVar = 8.44e+11;
-    char charVal = 'W';
+    float x = 331.79;
 
-    _Bool boolVar = 0;
-
-    printf("integerVal = %i\n", integerVal);
-    printf("floatingVal = %f\n", floatingVal);
-    printf("doubleVar = %e\n", doubleVar);
-    printf("doubleVar= %g\n", doubleVar);
-    printf("charVal = %c\n", charVal);
-
-    printf("boolVar = %i\n", boolVar);
+    printf("floatingVal = %.2f\n", x);
 
     return 0; 
 }
