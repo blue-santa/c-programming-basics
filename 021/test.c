dc7f9cfb257eb7dfc0706659007f42b9843191d8
Siddhartha <siddhartha-crypto@protonmail.com>
2020-01-01 16:09:39 -0900

    initiate 021

---
+++ 021/test.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) { 
+
+    float hardData[5][12] = {
+        {5.9, 4.3, 1.2, 5.3, 5.5, 2.1, 3.6, 2.1, 1.2, 1.3, 9.7, 3.1},
+        {3.9, 2.3, 1.2, 5.3, 0.5, 2.1, 3.6, 7.1, 1.2, 1.3, 3.7, 2.1},
+        {5.9, 4.3, 1.4, 5.3, 5.1, 2.1, 0.6, 2.1, 2.8, 1.7, 3.1, 5.1},
+        {2.9, 4.3, 0.4, 5.3, 1.1, 2.1, 0.6, 2.1, 2.8, 1.5, 0.5, 5.1},
+        {1.5, 3.0, 2.9, 5.3, 3.7, 1.8, 0.6, 1.9, 2.1, 1.7, 7.1, 5.1},
+    };
+
+    float yearlyAverages[5] = {0};
+    float monthlyAverages[12] = {0};
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 12; j++) { 
+            monthlyAverages[j] += hardData[i][j];
+            yearlyAverages[i] += hardData[i][j];
+        }
+    }
+
+    float totalYearlyAverage;
+
+    printf("YEAR\t\tRAINFALL (INCHES)\n");
+    for (int i = 0; i < 5; i++) {
+        yearlyAverages[i] = yearlyAverages[i] / 12;
+        totalYearlyAverage += yearlyAverages[i];
+        printf("%d\t\t%.2f\n", 2010 + i, yearlyAverages[i]);
+    }
+
+    totalYearlyAverage = totalYearlyAverage / 5;
+    printf("\nTotal Year Average: %.3f\n", totalYearlyAverage);
+
+
+    printf("\nJAN\tFEB\tMAR\tAPR\tMAY\tJUN\tJUL\tAUG\tSEP\tOCT\tNOV\tDEC\n");
+
+    for (int j = 0; j < 12; j++) {
+        monthlyAverages[j] = monthlyAverages[j] / 5;
+        printf("%3.1f\t", monthlyAverages[j]);
+    }
+
+    printf("\n"); 
+
+    return 0; 
+}
